9b676923d8ec0dc0b57aeab1cbfd782d32a3df52
yasuaki640 <yasuaki640@gmail.com>
2021-03-14 17:13:58 +0900

    クエリストリングのバリデーション

--- backend/app/Http/Controllers/HelloController.php
+++ backend/app/Http/Controllers/HelloController.php
@@ -5,16 +5,32 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\HelloRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        return view('hello.index');
+        $validator = Validator::make($request->query(), [
+            'id' => 'required',
+            'pass' => 'required'
+        ]);
+        $msg = $validator->fails() ? 'failure' : 'success';
+        return view('hello.index', ['msg' => $msg]);
     }
 
-    public function post(HelloRequest $request)
+    public function post(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|between:0,150'
+        ]);
+        if ($validator->fails()) {
+            return redirect('/hello')
+                ->withErrors($validator)
+                ->withInput();
+        }
         return view('hello.index', ['msg' => 'validation success']);
     }
 }
