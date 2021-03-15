ae51268c1ddfd3c9322b5f0cd6231487055e59b5
yasuaki640 <yasuaki640@gmail.com>
2021-03-15 17:51:38 +0900

    DBクラス、getAll的なやつ

--- backend/app/Http/Controllers/HelloController.php
+++ backend/app/Http/Controllers/HelloController.php
@@ -5,18 +5,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\HelloRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        if ($request->hasCookie('msg')) {
-            $msg = 'Cookie: ' . $request->cookie('msg');
-        } else {
-            $msg = '※No cookie exists';
-        }
-        return view('hello.index', ['msg' => $msg]);
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items' => $items]);
     }
 
     public function post(Request $request)
--- backend/config/database.php
+++ backend/config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('jdbc:mysql://localhost:3306/laravel_local'),
+            'host' => env('DB_HOST', 'db'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'laravel_local'),
+            'username' => env('DB_USERNAME', 'phper'),
+            'password' => env('DB_PASSWORD', 'secret'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
