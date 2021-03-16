3a4830259a9e1372a886b5be0968e7bee098bcf3
yasuaki640 <yasuaki640@gmail.com>
2021-03-17 08:45:07 +0900

    Eloquent ::all

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -25,6 +25,7 @@ Route::post('hello/add', 'App\Http\Controllers\HelloController@create');
 Route::get('hello/edit', 'App\Http\Controllers\HelloController@edit');
 Route::post('hello/edit', 'App\Http\Controllers\HelloController@update');
 Route::get('hello/show', 'App\Http\Controllers\HelloController@show');
+Route::get('person', 'App\Http\Controllers\PersonController@index');
 
 
 
